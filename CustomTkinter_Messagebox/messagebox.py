87de2bf44ebf23a588471e0a2d2b074bc5efef3c
Jorge <jorgejapao00@gmail.com>
2024-05-10 23:23:23 -0300

    Option to turn off audio added

--- CustomTkinter_Messagebox/messagebox.py
+++ CustomTkinter_Messagebox/messagebox.py
@@ -2,21 +2,26 @@ import customtkinter as ct
 import winsound
 from PIL import Image
 
-def open():
+def open_sound():
     system_sound_path = "C:\\Windows\\Media\\Windows Notify System Generic.wav"
     winsound.PlaySound(system_sound_path, winsound.SND_FILENAME | winsound.SND_ASYNC)
 
-def messagebox(title='Aviso', text='Placeholder'):
+def messagebox(title='Aviso', text='Placeholder', sound='on'):
     message_box = ct.CTkToplevel()
     message_box.geometry('320x150')
     message_box.title(title)
     message_box.resizable(False, False)
-    message_box.attributes('-toolwindow', False, '-topmost', True)
+    message_box.attributes('-toolwindow', True, '-topmost', True)
     message_box.grab_set()
 
 
     l1 = ct.CTkLabel(message_box, text=text)
     l1.pack(pady=30)
+
+
+    my_image = ct.CTkImage(light_image=Image.open(r"Visual\Icons\close.png"), dark_image=Image.open(r"Visual\Icons\close.png"),size=(30, 30))
+
+    i1 = ct.CTkLabel(message_box, image=my_image, text="")
 
     colored_frame = ct.CTkFrame(message_box, height=1)
     colored_frame.pack(side="bottom", fill="x")
@@ -24,14 +29,16 @@ def messagebox(title='Aviso', text='Placeholder'):
     b1 = ct.CTkButton(colored_frame, text='Ok', command=message_box.destroy)
     b1.pack(pady=15)
 
-    open()
+    if sound == 'on':
+        open_sound()
+    else:
+        pass
 
 
 
 root = ct.CTk()
 root.geometry("300x200")
 
-# Botão para mostrar a caixa de diálogo personalizada
 button = ct.CTkButton(root, text="Test Button", command=lambda:messagebox(title='Warning!', text='Missing arg'))
 button.pack(pady=20)
 
