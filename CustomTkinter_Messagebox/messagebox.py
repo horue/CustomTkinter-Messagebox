7fa0989f1c55b700a2f0c2f73c9c576640bf4d92
Jorge <jorgejapao00@gmail.com>
2024-05-11 11:53:47 -0300

    Function added to Messagebox class

--- CustomTkinter_Messagebox/messagebox.py
+++ CustomTkinter_Messagebox/messagebox.py
@@ -14,41 +14,43 @@ def open_sound():
     system_sound_path = "C:\\Windows\\Media\\Windows Notify System Generic.wav"
     winsound.PlaySound(system_sound_path, winsound.SND_FILENAME | winsound.SND_ASYNC)
 
-def messagebox(title='Warning!', text='Placeholder', sound='on', button_text='OK'):
-    message_box = ct.CTkToplevel()
-    message_box.geometry('320x150')
-    message_box.title(title)
-    message_box.resizable(False, False)
-    message_box.attributes('-toolwindow', True, '-topmost', True)
-    message_box.grab_set()
-    center_window(message_box)
+
+class Messagebox():
+    def CTkMessagebox(title='Warning!', text='Placeholder', sound='on', button_text='OK'):
+        message_box = ct.CTkToplevel()
+        message_box.geometry('320x150')
+        message_box.title(title)
+        message_box.resizable(False, False)
+        message_box.attributes('-toolwindow', True, '-topmost', True)
+        message_box.grab_set()
+        center_window(message_box)
 
 
-    l1 = ct.CTkLabel(message_box, text=text)
-    l1.pack(pady=30)
+        l1 = ct.CTkLabel(message_box, text=text)
+        l1.pack(pady=30)
 
 
-    my_image = ct.CTkImage(light_image=Image.open(r"Visual\Icons\close.png"), dark_image=Image.open(r"Visual\Icons\close.png"),size=(30, 30))
+        my_image = ct.CTkImage(light_image=Image.open(r"Visual\Icons\close.png"), dark_image=Image.open(r"Visual\Icons\close.png"),size=(30, 30))
 
-    i1 = ct.CTkLabel(message_box, image=my_image, text="")
+        i1 = ct.CTkLabel(message_box, image=my_image, text="")
 
-    colored_frame = ct.CTkFrame(message_box, height=1)
-    colored_frame.pack(side="bottom", fill="x")
+        colored_frame = ct.CTkFrame(message_box, height=1)
+        colored_frame.pack(side="bottom", fill="x")
 
-    b1 = ct.CTkButton(colored_frame, text=button_text, command=message_box.destroy)
-    b1.pack(pady=15)
+        b1 = ct.CTkButton(colored_frame, text=button_text, command=message_box.destroy)
+        b1.pack(pady=15)
 
-    if sound == 'on':
-        open_sound()
-    else:
-        pass
+        if sound == 'on':
+            open_sound()
+        else:
+            pass
 
 
 
 root = ct.CTk()
 root.geometry("300x200")
 
-button = ct.CTkButton(root, text="Test Button", command=lambda:messagebox(title='Warning!', text='Error. No link was given.'))
+button = ct.CTkButton(root, text="Test Button", command=lambda:Messagebox.CTkMessagebox(title='Warning!', text='Error. No link was given.'))
 button.pack(pady=20)
 
 root.mainloop()
