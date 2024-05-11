930b2bcd4b2a1d9f2b6b3bd480c0db4e26d893ad
Jorge <jorgejapao00@gmail.com>
2024-05-10 22:09:21 -0300

    Gramatical correctin

--- CustomTkinter_Messagebox/messagebox.py
+++ CustomTkinter_Messagebox/messagebox.py
@@ -1,21 +1,26 @@
 import customtkinter as ct
 
-def messagebox(title='Aviso'):
+def messagebox(title='Aviso', text='Placeholder'):
     message_box = ct.CTkToplevel()
     message_box.geometry('320x150')
     message_box.title(title)
     message_box.resizable(False, False)
     message_box.attributes('-toolwindow', True, '-topmost', True)
 
+    l1 = ct.CTkLabel(message_box, text=text)
+    l1.pack(pady=30)
+
     b1 = ct.CTkButton(message_box, text='Ok', command=message_box.destroy)
-    b1.pack()
+    b1.pack(pady=15)
+
+
 
 
 root = ct.CTk()
 root.geometry("300x200")
 
 # Botão para mostrar a caixa de diálogo personalizada
-button = ct.CTkButton(root, text="Mostrar Aviso", command=lambda:messagebox(title='Aviso!'))
+button = ct.CTkButton(root, text="Test Button", command=lambda:messagebox(title='Warining!'))
 button.pack(pady=20)
 
 root.mainloop()
